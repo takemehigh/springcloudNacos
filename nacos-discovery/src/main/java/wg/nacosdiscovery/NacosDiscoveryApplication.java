95b34fdb3c9fc293999e3b1d1d7ae5af09ab38ba
370503306@qq.com <1994wangge>
2021-07-27 00:39:47 +0800

    ok

---
+++ nacos-discovery/src/main/java/wg/nacosdiscovery/NacosDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package wg.nacosdiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacosDiscoveryApplication.class, args);
+    }
+
+}
