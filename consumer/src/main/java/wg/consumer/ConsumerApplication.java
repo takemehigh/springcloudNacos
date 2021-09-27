f97db9585bbb4923c3b5a404ec3cd4d01b6309ed
wg <370503306@qq.com>
2021-09-27 16:38:02 +0800

    123

--- consumer/src/main/java/wg/consumer/ConsumerApplication.java
+++ consumer/src/main/java/wg/consumer/ConsumerApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +21,16 @@ import org.springframework.web.client.RestTemplate;
 public class ConsumerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConsumerApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(ConsumerApplication.class, args);
+
+        Environment env = applicationContext.getEnvironment();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        System.out.println("\n--------------------------------------\n\t" +
+                "Application is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port + path+ "/index.html\n\t" +
+                "swagger-ui: \thttp://localhost:" + port + path + "/swagger-ui.html\n\t" +
+                "----------------------------------------------------------");
     }
 
     @Configuration
--- provider/src/main/java/wg/provider/ProviderApplication.java
+++ provider/src/main/java/wg/provider/ProviderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import com.alibaba.nacos.api.naming.NamingMaintainService;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ProviderApplication {
